0ad62ad7be7326df8e7bd7ac28d25b696ea0e18e
Mateus Borges <borgesm.work@gmail.com>
2022-11-23 19:26:25 -0300

    refactor: delete create-react-app unused files, add folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import ProductsPage from './pages/ProductsPage/ProductsPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<ProductsPage />} />
+				<Route path='/sign-up' element={<SignUpPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
