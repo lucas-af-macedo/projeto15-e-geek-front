f7c4c128e7a89acc2b921f8f8f0478b99a61515a
Lucas Macedo <lucasamacedo@hotmail.com>
2022-11-25 21:12:37 -0300

    feat!: create sign in page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ProductsPage from './pages/ProductsPage/ProductsPage';
+import SignInPage from './pages/SignInPage/SignInPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import GlobalStyle from './assets/css/GlobalStyle'
 
 function App() {
 	return (
 		<BrowserRouter>
+			<GlobalStyle/>
 			<Routes>
 				<Route path='/' element={<ProductsPage />} />
 				<Route path='/sign-up' element={<SignUpPage />} />
+				<Route path='/sign-in' element={<SignInPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
