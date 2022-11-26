de88da8045c21630a07724c2c48057a62ed1d13c
Mateus Borges <borgesm.work@gmail.com>
2022-11-25 21:57:38 -0300

    fix: remove double declaration of GlobalStyle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ProductsPage from './pages/ProductsPage/ProductsPage';
 import SignInPage from './pages/SignInPage/SignInPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import SingleProductPage from './pages/SingleProductPage/SingleProductPage.js';
-import GlobalStyle from './assets/css/GlobalStyle.js'
 
 function App() {
 	return (
@@ -17,7 +16,7 @@ function App() {
 				<Route path='/' element={<ProductsPage />} />
 				<Route path='/sign-up' element={<SignUpPage />} />
 				<Route path='/sign-in' element={<SignInPage />} />
-        <Route path='/product/:id' element={<SingleProductPage />} />
+				<Route path='/product/:id' element={<SingleProductPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
